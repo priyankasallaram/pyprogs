23db4b7cb7bdc9a714549f90b0bd2ba521312135
spli5nabeel <leeban99@gmail.com>
2021-06-21 20:31:15 +0530

    updated sqlite operations

--- db.py
+++ db.py
@@ -1,27 +1,46 @@
 import sqlite3
+import traceback
 
 
 def dbConnection():
-    conn = sqlite3.connect('test111.db')
-    print("Opened database successfully")
-    return conn
+    try:
+        conn = sqlite3.connect('ZscalerClient-versions.db')
+        print("Opened database successfully")
+        return conn
+    except Exception:
+        print("Error occured")
 
 def createBaseTable(conn):
-    conn.execute('''CREATE TABLE ZSCALER_VERSIONS
-             (ID INT PRIMARY KEY     NOT NULL,
-             VERSION TEXT NOT NULL,
-             REMARKS TEXT);''')
-    print("Table created successfully")
+    try:
+        conn.execute('''CREATE TABLE ZSCALER_VERSIONS
+                (VERSION TEXT NOT NULL,
+                TYPE TEXT NOT NULL,
+                CATEGORY TEXT NOT NULL);''')
+        print("Table created successfully")
+    except Exception:
+        print("Table already exists")
 
-def getMaxVersion():
-    return 
+def checkVersion(conn,version,type):
+    try:
+        cur = conn.cursor()
+        cur.execute("SELECT count(*) from ZSCALER_VERSIONS where VERSION = ? and TYPE = ?",(version,type,))
+        return cur.fetchone()
+    except Exception:
+        print("Problem with checkVersion")
+    
 
-def recordNewVersion(conn, id, version, remarks):
-    sql = "INSERT INTO ZSCALER_VERSIONS(ID,VERSION,REMARKS)values(?,?,?)"
-    cur = conn.cursor()
-    cur.execute(sql,(id,version,remarks))
-    conn.commit()
-    return
+def recordNewVersion(conn, version, type, category):
+    try:
+        cur = conn.cursor()
+        cur.execute("INSERT INTO ZSCALER_VERSIONS(VERSION,TYPE,CATEGORY)values(?,?,?)",(version,type,category))
+        conn.commit()
+        return
+    except Exception:
+        print("Error Occured in recording new version")
+        traceback.print_exc()
 
 def closeConnection(conn):
-    conn.close()
+    try:
+        conn.close()
+    except Exception:
+        print("Error Occured in closing connection")
--- version_history.py
+++ version_history.py
@@ -1,7 +1,21 @@
-from db import closeConnection, createBaseTable, dbConnection
+from db import checkVersion, closeConnection, createBaseTable, dbConnection, recordNewVersion
 import sqlite3
 
+version_mac = "1.2.1.1"
+version_win = "1.2.1.11"
 conn = dbConnection()
 createBaseTable(conn)
+id = checkVersion(conn,version_mac,"mac")
+print(id[0])
+if id[0] == 0:
+    recordNewVersion(conn,version_mac,"mac","Not Available")
+else:
+    print("Version is already available with us")
+id = checkVersion(conn,version_win,"mac")
+print(id[0])
+if id[0] == 0:
+    recordNewVersion(conn,version_win,"mac","Not Available")
+else:
+    print("Version is already available with us")
 closeConnection(conn)
 
